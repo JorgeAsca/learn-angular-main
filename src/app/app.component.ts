52fa8ac8773cd0577f142ed315515bd527b336ba
Jorgeasca <jascola171@g.educaand.es>
2025-04-11 16:47:49 +0200

    Punto 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,31 @@
 import {Component} from '@angular/core';
-import {UserComponent,} from './user.component';
+// import {UserComponent,} from './user.component';
 
 @Component({
   selector: 'app-root',
-  styleUrls: ['app.component.css'],
+  // styleUrls: ['app.component.css'],
   // Punto uno meodificado de template 
   template: `
-    <!-- punto dos --->
-    username: {{ username }} 
-    Hello {{ city }}, {{1 + 1}}
-  `,
+  <!-- Punto 4 flujo de control entre  comonentes -->
+  @if (isServerRunning) {
+    <span>Server is running</span>
+  } @else {
+    <span>Server is not running</span>
+  }
+   `,
+    // <!-- punto dos --->
+    // username: {{ username }} 
+    // Hello {{ city }}, {{1 + 1}}
+  
 
   
-  imports: [UserComponent],
+  // <!-- imports: [UserComponent], -->
 })
 
 // punto dos agregando una propiedad a la clase
 export class AppComponent {
+  // Se agrego la propiedad isServerRunning para el punto 4
+  isServerRunning = true;
   username = 'Jorge asca';
   city = 'San Francisco';
 }
