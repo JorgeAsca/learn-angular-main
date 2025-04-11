d5eae8c27015130c781827da6b5d9a20a098e4ad
Jorgeasca <jascola171@g.educaand.es>
2025-04-11 16:05:38 +0200

    ejercicios

---
+++ src/app/puntouno/puntouno.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-puntouno',
+  imports: [],
+  template: `
+    <p>
+      puntouno works!
+    </p>
+  `,
+  styles: ``
+})
+export class PuntounoComponent {
+
+}
